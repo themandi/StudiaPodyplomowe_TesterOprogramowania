986128a96b8974d0782d738e31cf1bacd065d015
themandi <wudzinska.agnieszka@gmail.com>
2023-06-20 23:56:21 +0000

    Add new tests in the project

---
+++ TDD/1.py
@@ -0,0 +1,16 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_one(self):
+        pass
+    
+    def test(self):
+        pass
+
+class MyTestCase2(unittest.TestCase):
+    def test_two(self):
+        pass
+    def test_three(self):
+        pass
+if __name__ == "__main__":
+    unittest.main()
